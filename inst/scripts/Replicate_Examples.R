70b952b96ec9411df70a2631474d3a86cbc8cb9e
merolagio <merolagio@gmail.com>
2026-03-04 07:29:58 +0700

    cleaned

--- inst/scripts/Replicate_Examples.R
+++ inst/scripts/Replicate_Examples.R
@@ -14,6 +14,7 @@ ms_scalesh_names = unique(ms_lookup$scaleSH)
 
 ms_n = nrow(msscq)
 ms_p = ncol(msscq)
+
 ms_r = cor(msscq)
 
 plotcor(ms_r, groups = ms_scalesh_fac,   separate_groups = T, axis_labels = F, add_group_names = T, expandTop = 0.05, group_names_vjust = -0.05)
@@ -31,26 +32,25 @@ wachterqq(ms_ee$values, p = ms_p, n = ms_n, cor = T, nfit_line = -4)
 ms_pca = pca(msscq, ncomps = 4)
 summary(ms_pca)
 
-# plot contributions
-ms_pca_contr_pl = plot(ms_pca, returnplot = T, vargroups = ms_scalesh_fac, varnames = F, colourscale = "ggplot", legendPosition = "right", stripnames = paste("Component", 1:4), produceplot = F)
+## some cardinalities are < 99 because the threshold for zero (thrsehcard) is set to 0.001
 
-ms_pca_contr_pl
+# plot contributions
+plot(ms_pca, returnplot = T, vargroups = ms_scalesh_fac, varnames = F, colourscale = "ggplot", legendPosition = "right", stripnames = paste("Component", 1:4), produceplot = F)
+
 
 ##SPCA===================
 
 #slow 17k obs x 100 vars hard for leaps::regsubsets
 
-#timesta = Sys.time()
 ms_pspcas95 = PMlsspca::lsspca(msscq, alpha = 0.95, ncomps = 4,  method = "p", varselection = "s", scalex = FALSE) 
 
 summary(ms_pspcas95, variance_metrics = "both")
 
 plot(ms_pspcas95, nplot = 4, onlynonzero = T, varnames = F, x_axis_lab = "item", legendPosition = "right", vargroups = ms_scalesh_fac, colourscale = "ggplot")  
 
-
 # correlation table
-tab_cors = round(rbind(ms_pspcas95$corComp, diag(cor(ms_pca$scores, ms_pspcas95$scores))), 2)
-tab_cors
+round(rbind(ms_pspcas95$corComp, diag(cor(ms_pca$scores, ms_pspcas95$scores))), 2)
+
 
 #CRIME=================
 
@@ -61,26 +61,23 @@ cr_n = nrow(crime_data)
 
 cr_r = cor(crime_data)
 
-#scree and qq-plot===========
 
-##CLUSTER======================
+##Cluster variables======================
 cr_hc <- stats::hclust(as.dist(1 - abs(cr_r)), method = "average")
 cr_hcord <- cr_hc$order
 
 crhc = crime_data[, cr_hcord]
 crhc_r = cor(crhc)
+
+##Correlation plot===========
 crhc_cor_pl = plotcor(crhc_r, rtn = T, axis_labels = F)
 
-
+#scree and qq-plot===========
 cr_ee = eigen(cr_r)
-
 screeplot(cr_ee$values, kaiser_line = T)
-
-
 wachterqq(cr_ee$values, p = cr_p, n = cr_n, cor = T, nfit_line = -4)
 
 ##PCA===============
-
 cr_pca = pca(crime_data, ncomps = 4)
 summary(cr_pca)
 
@@ -90,14 +87,10 @@ cr_pspcas95 = lsspca(crime_data, alpha = 0.95, ncomps = 4,  method = "p", varsel
 
 ##tables====================
 
-###           summaries 
+### summaries 
 su = summary(cr_pspcas95, variance_metrics = "both", rtn = T, prn = F)
-PCs = c(diag(cor(cr_pca$scores, cr_pspcas95$scores)))
-round(rbind(su, PCs), 2)
+cors = c(diag(cor(cr_pca$scores, cr_pspcas95$scores)))
+round(rbind(su, "Cor PCs" = cors), 2)
 
-### print contributions 
-tab_contr = cr_pspcas95$contributions[, 1:2]
-ind = rowSums(tab_contr) != 0
-tab_contr = tab_contr[ind, ]
-
+### contributions 
 print(cr_pspcas95, cols = 1:2)
